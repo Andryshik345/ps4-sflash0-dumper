41939118c5eaff17504630854a27b367031da3df
Andryshik345 <andryshik9@gmail.com>
2024-08-24 20:47:05 +0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# PS4 Sflash0 NOR Dump Payload
+
+This payload was made for dumping NOR firmware (so called Sflash0) to USB drive instead using FTP.
+
+The compiled PS4 payload is available for download in the [release section](https://github.com/Andryshik345/ps4-sflash0-dumper/releases/).
+
+## How to compile for PS4
+
+1. Set up the PS4 payload SDK from https://github.com/Scene-Collective/ps4-payload-sdk.
+2. In the ps4-sflash0-dumper directory: `make clean && make` to compile.
---
+++ source/main.c
@@ -0,0 +1,60 @@
+#ifdef PS4
+#include "ps4.h"
+#endif
+
+int _main(struct thread *td) {
+    
+    
+    initKernel();
+    initLibc();
+    initPthread();
+    jailbreak();
+    initSysUtil();
+
+    copy_file("/dev/sflash0", "/data/sflash0.bin");
+    printf_notification("sflash0 copied!\nChecking file size...");
+
+    struct stat info;
+    int res = 0;
+
+    if (!stat("/data/sflash0.bin", &info)) {
+        if (info.st_size == 33554432) {
+            printf_notification("sflash0.bin is %d bytes\nDump is correct!", info.st_size);
+            res = 1;
+        }
+        else {
+            printf_notification("sflash0.bin is %d bytes\nDump is NOT correct!\nTry reboot and run the payload again!", info.st_size);
+            res = unlink("/data/sflash0.bin");
+            return 0;
+        }
+    }
+    else {
+        printf_notification("sflash0.bin doesn't exist!\nSomething gone wrong...");
+        return 0;
+    }
+
+    if (res == 1) {
+        printf_notification("Copying sflash0.bin to USB...");
+        int usbdir = open("/mnt/usb0/.dirtest", O_WRONLY | O_CREAT | O_TRUNC, 0777);
+        if (usbdir == -1) {
+            usbdir = open("/mnt/usb1/.dirtest", O_WRONLY | O_CREAT | O_TRUNC, 0777);
+            if (usbdir == -1) {
+                printf_notification("USB not found!\nOnly internal backup was done\n(/data/sflash0.bin)!");
+            }
+            else {
+                close(usbdir);
+                unlink("/mnt/usb1/.dirtest");
+                copy_file("/data/sflash0.bin", "/mnt/usb1/sflash0.bin");
+                printf_notification("Dump copied to USB1 successfully!");
+            }
+        }
+        else {
+            close(usbdir);
+            unlink("/mnt/usb0/.dirtest");
+            copy_file("/data/sflash0.bin", "/mnt/usb0/sflash0.bin");
+            printf_notification("Dump copied to USB0 successfully!");
+        }
+    }
+
+    return 0;
+}
